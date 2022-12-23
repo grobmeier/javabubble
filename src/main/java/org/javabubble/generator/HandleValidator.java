2fec4508e9982d0986b82f6d7511a1ffbfe7feb8
Christian Grobmeier <cg@grobmeier.de>
2022-12-23 11:58:49 +0100

    enabled basic multibubble building

--- src/main/java/org/javabubble/generator/HandleValidator.java
+++ src/main/java/org/javabubble/generator/HandleValidator.java
@@ -1,8 +1,10 @@
 package org.javabubble.generator;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Objects;
 
 import org.javabubble.generator.model.JavaBubble;
 import org.javabubble.generator.model.ModelLoader;
@@ -14,15 +16,20 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 public class HandleValidator {
 
 	public static void main(String[] args) throws IOException {
-		var validator = new PersonValidator();
-		var bubble = new ModelLoader(Path.of("../")).load();
-		var cleanedBubble = new JavaBubble(bubble.people().stream() //
-				.map(validator::validate) //
-				.toList());
-		try (var writer = Files.newBufferedWriter(Path.of("../javapeople.yaml"))) {
-			writer.write("# Please add new entries in alphabetic order of the name.\n");
-			var mapper = new ObjectMapper(new YAMLFactory());
-			mapper.writeValue(writer, cleanedBubble.people());
+		var file = new File("./bubbles");
+		if (file.isDirectory()) {
+			for (var bubbleFileName : Objects.requireNonNull(file.list())) {
+				var validator = new PersonValidator();
+				var bubble = new ModelLoader(Path.of("bubbles")).load(bubbleFileName);
+				var cleanedBubble = new JavaBubble(bubble.people().stream() //
+						.map(validator::validate) //
+						.toList());
+				try (var writer = Files.newBufferedWriter(Path.of("bubbles/" + bubbleFileName))) {
+					writer.write("# Please add new entries in alphabetic order of the name.\n");
+					var mapper = new ObjectMapper(new YAMLFactory());
+					mapper.writeValue(writer, cleanedBubble.people());
+				}
+			}
 		}
 	}
 
--- src/main/java/org/javabubble/generator/SiteGenerator.java
+++ src/main/java/org/javabubble/generator/SiteGenerator.java
@@ -1,7 +1,10 @@
 package org.javabubble.generator;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
 
 import org.javabubble.generator.model.ModelLoader;
 import org.javabubble.generator.site.FileOutput;
@@ -10,8 +13,15 @@ import org.javabubble.generator.site.Site;
 public class SiteGenerator {
 
 	public static void main(String[] args) throws IOException {
-		var bubble = new ModelLoader(Path.of("../")).load();
-		var output = new FileOutput(Path.of("target/site"));
-		new Site(bubble, output).generate();
+		var file = new File("./bubbles");
+		if (file.isDirectory()) {
+			for (var bubbleFileName : Objects.requireNonNull(file.list())) {
+				var bubbleName = bubbleFileName.replace(".yaml", "");
+
+				var bubble = new ModelLoader(Path.of("bubbles")).load(bubbleFileName);
+				var output = new FileOutput(Path.of("target/site/" +  bubbleName));
+				new Site(bubble, output).generate();
+			}
+		}
 	}
 }
